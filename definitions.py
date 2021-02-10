b0172fece506740546ab44011c6572b9346cc37d
ndgigliotti <ndgigliotti@gmail.com>
2021-02-09 20:49:31 -0500

    Change log dir

--- definitions.py
+++ definitions.py
@@ -2,8 +2,7 @@
 from os import path
 
 PROJECT_ROOT_DIR = path.dirname(path.abspath(__file__))
-RESOURCES_DIR = path.join(PROJECT_ROOT_DIR, 'resources')
-LOG_DIR = path.join(RESOURCES_DIR, "logs")
+LOG_DIR = path.join(PROJECT_ROOT_DIR, 'logs')
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 DATE_FORMAT = "%Y-%m-%d"
 DATETIME_FORMAT = "%Y-%m-%dT%H-%M-%S"
